3edccf2684017a4d554aefe3976c5cfbbed95a79
GroboChan <230193800+grobo-chan@users.noreply.github.com>
2026-09-11 18:08:18 +0530

    literally done now lmao

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # pk-random
-Bash Script to pick out a Random Headmate from your PluralKit
+Bash Script to pick out a Random Headmate from your PluralKit.\
+NOTE: The Script explicitly filters by publicly visbile headmates.
+This lets you set any "meme" proxies to private so they don't pop up in this.
+(Yes.... we have a few meme proxies lmao)
 
 # Dependencies
 - A PluralKit Token
@@ -7,3 +10,32 @@ Bash Script to pick out a Random Headmate from your PluralKit
 - `curl`
 - `jq`
 - `date`
+- `awk`
+
+# License
+Copyright (C) 2026 GroboChan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Running the program
+```
+git clone https://github.com/grobo-chan/pk-random
+cd pk-random
+bash ./script.sh [-b|--bias] [-t|--token TOKEN]
+```
+
+In --bias mode the headmate's who front less are more likely to be picked.\
+This program will require your PK Token. These are used to send requests to the PK API to fetch members and front history.\
+The token is NOT being sent anywhere else and is only used for the purpose of data fetching.\
+If token is not supplied via --token, the program will ask for one.
--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ while [[ $# -gt 0 ]]; do
             echo "Tiny script to pick a random headmate from PluralKit"
             echo "Usage: $0 [-b|--bias] [-t|--token TOKEN]"
             echo
-            echo "In bias mode the headmate's who front less are more likely to be picked."
+            echo "In --bias mode the headmate's who front less are more likely to be picked."
             echo "This program will require your PK Token. These are used to send requests to the PK API to fetch members and front history."
             echo "The token is NOT being sent anywhere else and is only used for the purpose of data fetching."
             echo "If token is not supplied via --token, the program will ask for one."
@@ -42,10 +42,17 @@ MEMBERS="$(curl -s -H "Authorization: $PK_TOKEN" https://api.pluralkit.me/v2/sys
 COUNT=$(echo "$MEMBERS" | jq 'length')
 
 if [[ "$BIAS" == "true" ]]; then
+    declare -A MEMBER_TIMES
+    # Get every member and set their front time to 0
+    while read i; do
+        MEMBER_TIMES["$i"]=0
+    done < <(echo "$MEMBERS" | jq -c .[].id)
+
     # Get past 100 swicthes
     DATA="$(curl -s -H "Authorization: $PK_TOKEN" https://api.pluralkit.me/v2/systems/@me/switches | jq -c .[])"
     PREV_TIMESTAMP=$(date +%s)
-    echo "$DATA" | while read i; do
+
+    while read i; do
         # jq doesn't support microseconds so we cut them out with crazy ass regex
         # We also need to convert the timestamp from ISO-8601 to Unix Timestamps
         CURR_TIMESTAMP=$(echo "$i" | jq '.timestamp | sub("\\.[0-9]+Z$"; "Z") | fromdateiso8601')
@@ -54,16 +61,78 @@ if [[ "$BIAS" == "true" ]]; then
         # For first member of array (fronter) is TIME_NOW - TIMESTAMP_IN_JSON
         # For rest it is PREV_TIMESTAMP - CURR_TIMESTAMP
         DELTA=$((PREV_TIMESTAMP - CURR_TIMESTAMP))
-        days=$(( DELTA / 86400 ))
-        hours=$(( (DELTA % 86400) / 3600 ))
-        minutes=$(( (DELTA % 3600) / 60 ))
-        seconds=$(( DELTA % 60 ))
 
-        echo "Front time for members $(echo "$i" | jq -c .members) is ${days}d ${hours}h ${minutes}m ${seconds}s"
+        # Make a bash Associative Array in the format of "pk_member_id": "front_time"
+        while read -r m; do
+            MEMBER_TIMES["$m"]=$((${MEMBER_TIMES["$m"]:-0} + DELTA))
+        done < <(echo "$i" | jq '.members[]')
 
         # Set the PREV_TIMESTAMP for next iteration
         PREV_TIMESTAMP="$CURR_TIMESTAMP"
+    done < <(echo "$DATA")
+
+    for k in "${!MEMBER_TIMES[@]}"; do
+        echo "$k ${MEMBER_TIMES[$k]}" >> "file.txt"
     done
+
+    # Scary ass awk script because awk lets us do weighted random
+    HEADMATE_ID=$(
+        for k in "${!MEMBER_TIMES[@]}"; do
+            echo "$k ${MEMBER_TIMES[$k]}"
+        done | awk '
+            {
+                keys[NR] = $1;
+                vals[NR] = $2;
+            }
+            END {
+                n = NR;
+                if (n == 0) exit;
+
+                # So, for our weighted random, the weights are determined by 1-PERCENTILE
+                # Where PERCENTILE = (HEADMATE_FRONT_TIME - MIN_FRONT_TIME) / (MAX_FRONT_TIME - MIN_FRONT_TIME)
+
+                # Get MIN_FRONT_TIME & MAX_FRONT_TIME
+                min_v = vals[1];
+                max_v = vals[1];
+                for (i = 2; i <= n; i++) {
+                    if (vals[i] < min_v) min_v = vals[i];
+                    if (vals[i] > max_v) max_v = vals[i];
+                }
+
+                range = max_v - min_v;
+                srand();
+                total_weight = 0;
+
+                # Calculate the weights (1 - PERCENTILE)
+                for (i = 1; i <= n; i++) {
+                    if (range == 0) {
+                        weights[i] = 1; # All values are identical
+                    } else {
+                        weights[i] = 1.0 - ((vals[i] - min_v) / range);
+                    }
+                    total_weight += weights[i];
+                }
+
+                # Let the randomizer do its thing
+                r = rand() * total_weight;
+                sum = 0;
+                for (i = 1; i <= n; i++) {
+                    sum += weights[i];
+                    if (r <= sum) {
+                        print keys[i]; # This is the Member ID in PK
+                        exit;
+                    }
+                }
+
+                print keys[n];
+            }
+        '
+    )
+
+    # Filter the JSON by Member ID and get the Display Name
+    HEADMATE=$(echo "$MEMBERS" | jq --argjson id "$HEADMATE_ID" '.[] | select(.id == $id) | .display_name')
+    echo "The headmate selected is: $HEADMATE"
+
 else
     # Pick a random member
     rand_idx=$(( RANDOM % COUNT ))
